8ccb9b4fb4d31761642522b70293cdb842015655
nichokevin <nicholaskevin28@gmail.com>
2020-04-30 18:41:37 +0700

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { JawabanComponent } from './jawaban/jawaban.component';
+import { HasilComponent } from './hasil/hasil.component';
 
+import {Routes, RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+const ROUTES: Routes=[
+  {path:'hasil', component: HasilComponent},
+  {path:'home', component: HomeComponent}
+]
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, JawabanComponent ],
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot(ROUTES)],
+  declarations: [ AppComponent, HelloComponent, HasilComponent, HomeComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
